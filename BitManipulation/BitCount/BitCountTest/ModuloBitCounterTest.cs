a74c4ac52afc14fedae95b4e61ca2e9fe4e50e2b
Florin <florin_grigoriu@yahoo.com>
2013-06-29 09:29:35 -0700

    Add support for modulo bit counter - complexity log(log(n))

---
+++ BitManipulation/BitCount/BitCountTest/ModuloBitCounterTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitCount;
+using System.Numerics;
+using System.Linq;
+
+namespace BitCountTest
+{
+    [TestClass]
+    public class ModuloBitCounterTest : BitCounterBaseTest
+    {
+        protected override IBitCounter GetCounter()
+        {
+            return new ModuloBitCounter();
+        }
+
+        [TestMethod]
+        public void TestMaskGenerations()
+        {
+            // get masks
+            var masks = new ModuloBitCounter().ComputeMasks(31);
+
+            // verify them
+            Assert.AreEqual(3, masks.Length);
+            Assert.IsTrue(masks.SequenceEqual(new BigInteger[]{0x55555555,0x33333333,0x0f0f0f0f}));
+        }
+     }
+}
