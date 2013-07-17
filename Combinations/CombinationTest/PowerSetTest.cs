9ffd978d10ec71f66724e82384d2408bbdc1a907
Florin <florin_grigoriu@yahoo.com>
2013-07-16 22:47:29 -0700

    Add support for power set combinations

---
+++ Combinations/CombinationTest/PowerSetTest.cs
@@ -0,0 +1,25 @@
+﻿using Combinations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace CombinationTest
+{
+    [TestClass]
+    public class PowerSetTest
+    {
+        [TestMethod]
+        public void TestPowerset()
+        {
+            // setup
+            var input = new char[] { 'c', 'b', 'a' };
+            var expectedOutput = new String[] { "", "c", "b", "cb", "a", "ca", "ba", "cba" };
+
+            // trigger
+            var result = PowerSet.GetPowerSet(input).Select(i=> new String(i.ToArray())).ToArray();
+
+            // validate
+            Assert.IsTrue(expectedOutput.SequenceEqual(result));
+        }
+    }
+}
