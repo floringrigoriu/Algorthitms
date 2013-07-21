f5c3f589daa60f5bc8003acaac1df907b18e2e5c
Florin <florin_grigoriu@yahoo.com>
2013-07-20 23:24:02 -0700

    Introduce Dutch flag implementation

---
+++ Sorting/Sorting/DutchFlagTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingImplementations;
+using System.Collections.Generic;
+
+namespace Sorting
+{
+    [TestClass]
+    public class DutchFlagTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            //get input
+            int testSize = 1000;
+            int lowSize = 300;
+            int uppSize = 400;
+            int low = 2;
+            int middle = 3;
+            int upper = 4;
+            int[] input = GetInput<int>(testSize, lowSize, uppSize, low, middle, upper);
+            
+            // apply dutch flag sorting
+            var result = DutchFlag.DutchPartition<int>(input, middle, new IntComparer()); 
+
+            // veryfi
+            Assert.AreEqual<int>(lowSize ,result.Item1);
+            Assert.AreEqual<int>(uppSize + result.Item2 + 1, input.Length);
+            for (int index = 0; index < lowSize; index++)
+            {
+                Assert.AreEqual<int>(low, input[index], "invalid lower @ {0} : {1}", index , input[index]);
+            }
+
+            for (int index = result.Item1; index <= result.Item2; index++)
+            {
+                Assert.AreEqual<int>(middle, input[index], "invalid middle @ {0} : {1}", index, input[index]);
+            }
+
+            for (int index = result.Item2+1; index < input.Length; index++)
+            {
+                Assert.AreEqual<int>(upper, input[index], "invalid upper  @ {0} : {1}", index, input[index]);
+            }
+        }
+
+        private T[] GetInput<T>(int testSize, int lowSize, int uppSize, T low, T middle, T upper)
+        {
+            // Create a duthc flag
+            //
+            T[] result = new T[testSize];
+            for (int index = 0; index < lowSize; index++)
+            {
+                result[index] = low;
+            }
+
+            for (int index = lowSize; index < testSize - uppSize ; index++)
+            {
+                result[index] = middle;
+            }
+
+            for (int index = testSize - uppSize; index < testSize ; index++)
+            {
+                result[index] = upper;
+            }
+
+            // randomize it
+            //
+            Random randomizer = new Random();
+            for (int index = 0; index < testSize; index++)
+            {
+                var randomTarget = randomizer.Next(0, testSize);
+                T aux = result[randomTarget];
+                result[randomTarget] = result[index];
+                result[index] = aux;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// implementation of comparer for .net is missing - so adding a custom implementation, using 
+        /// the fact int.32 expands IComparable<int>
+        /// </summary>
+        private class IntComparer:IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return x.CompareTo(y);
+            }
+        }
+    }
+}
---
+++ Sorting/SortingImplementations/DutchFlag.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace SortingImplementations
+{
+    /// <summary>
+    /// support for Dutch flag organization
+    /// an array will be split in 3 section 
+    /// >> smaler than a target
+    /// >> equal to a target
+    /// >> bigger than the target
+    /// </summary>
+    /// <remarks>this organization improves performance of quick sort of arrays with many duplicates</remarks>
+    public class DutchFlag
+    {
+        /// <summary>
+        /// Dutch flag for the input with defaulting to 0 for start and full array as end
+        /// </summary>
+        /// <typeparam name="T">type of array elements</typeparam>
+        /// <param name="input">input array</param>
+        /// <param name="target">middle of dutch flag</param>
+        /// <param name="lower">lower boundary - defaulted to 0 [inclusive]</param>
+        /// <returns>tupple with the boundaries between the 3 sections of the flag</returns>
+        /// <remarks>inline for perfomance</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Tuple<int, int> DutchPartition<T>(T[] input, T target, IComparer<T> comparer, int lower = 0)
+        {
+            return DutchPartition<T>(input, target,comparer, lower, input.Length-1);
+        }
+
+        /// <summary>
+        /// Dutch flag for the input
+        /// </summary>
+        /// <typeparam name="T">type of array elements</typeparam>
+        /// <param name="input">input array</param>
+        /// <param name="target">middle of dutch flag</param>
+        /// <param name="lower">lower boundary - inlusive </param>
+        /// <param name="upper">upper boundary - inclusive </param>
+        /// <returns>tupple with the boundaries between the 3 sections of the flag</returns>
+        public static Tuple<int, int> DutchPartition<T>(T[] input, T target, IComparer<T> comparer, int lower, int upper)
+        {
+            // implementation concat smaller elements to lower part, 
+            // concat bigger elemments to upper part of the arry
+            // override middle with target
+
+            // where read/write low
+            var writeLow = lower;
+            var readLow = lower;
+
+            // where read/write high
+            var writeUpper = upper;
+            var readUpper = upper;
+
+            while (readLow < readUpper)
+            {
+                while (comparer.Compare(input[readLow],target) <=0 && readLow <= readUpper)
+                {
+                    if (comparer.Compare(input[readLow], target) != 0)
+                    {
+                        input[writeLow] = input[readLow];
+                        writeLow++;
+                    }
+                    readLow++;
+                }
+
+                while (comparer.Compare(input[readUpper], target) >= 0 && readLow <= readUpper)
+                {
+                    if (comparer.Compare(input[readUpper], target) != 0)
+                    {
+                        input[writeUpper] = input[readUpper];
+                        writeUpper--;
+                    }
+                    readUpper--;
+                }
+
+                if (readLow < readUpper) 
+                {
+                    var temp = input[readLow];
+                    input[readLow] = input[readUpper]; ;
+                    input[readUpper] = temp;
+                }
+            }
+
+
+            for (int index = writeLow; index <=writeUpper; index++)
+            {
+                input[index] = target;
+            }
+
+            return new Tuple<int, int>(writeLow, writeUpper);
+        }
+        
+    }
+}
