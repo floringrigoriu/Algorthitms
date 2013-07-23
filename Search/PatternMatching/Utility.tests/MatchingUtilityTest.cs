bca882f7d2a3bb0be497008f3edcfb1fec90d28b
Florin <florin_grigoriu@yahoo.com>
2013-07-22 22:26:13 -0700

    Pattern matching - dynamic programming

---
+++ Search/PatternMatching/Utility.tests/MatchingUtilityTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Utility.tests
+{
+    [TestClass]
+    public class MatchingUtilityTest
+    {
+        Dictionary<String, String> matching = new Dictionary<string, string>()
+        {
+            {"* is *", "falling is learning"},
+            {"i*", "iFone"},
+            {"i *", "I am free"},
+            {"To * is like having a * for breakfast", 
+             "To pickle is like having a glass of vinegar for breakfast"}
+        };
+
+        Dictionary<String, String> notMatching = new Dictionary<string, string>()
+        {
+            {"* is  *", "falling is learning"},
+            {"i*", " iFone"},
+            {"i *", "I'm free"},
+            {"To * is like having a * for breakfast", "To pickle is like having a glass of vinegar for breakfast and lunch"}
+        };
+
+        Dictionary<String, Token[]> tokensDictionary = new Dictionary<string, Token[]>()
+        {
+            {"* is *", new Token[] {new Token(" is ")}},
+            {"i*", new Token[] {new Token("i",true)}},
+            {"i *", new Token[] {new Token("i ",true)}},
+            {"To * is like having a * for breakfast", new Token[] 
+                {new Token("To ",true), new Token(" is like having a "),new Token(" for breakfast",false,true)}}
+        };
+
+        MatchingUtility utility = new MatchingUtility();
+
+        [TestMethod]
+        public void TokenizeTest()
+        {
+            foreach (var kvp in this.tokensDictionary)
+            { 
+                // trigger
+                var computedTokens = this.utility.Tokanize(kvp.Key);
+
+                // validate
+                Assert.IsTrue(computedTokens.SequenceEqual(kvp.Value),
+                    "Unexpected tokanization. discovered {1} {0} {1} while expedted{1} {2}",
+                    String.Concat(computedTokens.Select(c => c.ToString())),
+                    Environment.NewLine,
+                    String.Concat(kvp.Value.Select(c => c.ToString()))
+                    );
+            }
+        }
+
+        [TestMethod]
+        public void MatchTest()
+        {
+            foreach (var kvp in this.matching)
+            {
+                // trigger
+                var areMatching = this.utility.Matches(kvp.Key,kvp.Value);
+
+                // validate
+                Assert.IsTrue(areMatching, "Expected '{0}' to match template '{1}'",kvp.Value , kvp.Key);
+            }
+
+            foreach (var kvp in this.notMatching)
+            {
+                // trigger
+                var areMatching = this.utility.Matches(kvp.Key, kvp.Value);
+
+                // validate
+                Assert.IsFalse(areMatching, "Expected '{0}' not to match template '{1}'", kvp.Value, kvp.Key);
+            }
+        }
+
+        [TestMethod]
+        public void PerformanceTest()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            // test 10 million matches 1250000 * 8
+            for (int iteration = 0; iteration < 1250000; iteration++)
+            {
+                foreach (var kvp in this.matching)
+                {
+                    // trigger
+                    var areMatching = this.utility.Matches(kvp.Key, kvp.Value);
+                }
+
+                foreach (var kvp in this.notMatching)
+                {
+                    // trigger
+                    var areMatching = this.utility.Matches(kvp.Key, kvp.Value);
+                }
+            }
+            watch.Stop();
+            Assert.IsTrue(watch.ElapsedMilliseconds < 1000, "Time : {0}", watch.Elapsed.ToString());
+        }
+
+    }
+}
---
+++ Search/PatternMatching/Utility/MatchingUtility.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class MatchingUtility
+    {
+        /// <summary>
+        /// comparison between strings
+        /// </summary>
+        private StringComparison comparison;
+
+        private Dictionary<string, List<Token>> cachedTokens = new Dictionary<string, List<Token>>();
+
+        /// <summary>
+        /// Build a custom comparer utility
+        /// </summary>
+        /// <param name="comparer">comparison provider</param>
+        public MatchingUtility(StringComparison comparer = StringComparison.InvariantCultureIgnoreCase)
+        {
+            this.comparison = comparer;
+        }
+
+        public bool Matches(String pattern, String input)
+        {
+            Contract.Assert(pattern != null);
+            Contract.Assert(input != null);
+            // 1. Tokenize
+            // 2. Matches
+
+            int start = 0;
+
+            foreach (var token in Tokanize(pattern))
+            {
+                int firstOccurence = input.IndexOf(token.TokenValue, start, this.comparison);
+                if (firstOccurence == -1  || // not found
+                    (token.IsStarting &&  firstOccurence != 0) ||  // not at beging for search
+                    (token.IsEnding &&  firstOccurence  +  token.TokenValue.Length != input.Length ))  // not at end for search
+                {
+                    return false;
+                }
+
+                // prepare next search;
+                start = firstOccurence + token.TokenValue.Length;
+            }
+
+            return true;
+        }
+
+        public IEnumerable<Token> Tokanize(String pattern)
+        {
+            Contract.Assert(pattern != null);
+            if (this.cachedTokens.ContainsKey(pattern))
+            {
+                foreach (var token in this.cachedTokens[pattern])
+                {
+                    yield return token;
+                }
+                yield break;
+            }
+            else
+            {
+
+
+                List<Token> currentResult = new List<Token>();
+
+
+                int start = 0;
+                for (int end = 0; end < pattern.Length; end++)
+                {
+                    if (pattern[end] == '*')
+                    {
+                        if (end > 0)
+                        {
+                            var token = new Token(pattern.Substring(start, end - start), start == 0);
+                            yield return token;
+                            currentResult.Add(token);
+                        }
+                        start = end + 1;
+                    }
+                }
+                if (pattern[pattern.Length - 1] != '*')
+                {
+                    var token = new Token(pattern.Substring(start), start == 0, true);
+                    yield return token;
+                    currentResult.Add(token);
+                }
+                this.cachedTokens[pattern] = currentResult;
+            }
+        }
+    }
+
+    public class Token
+    {
+        public Token(String value, bool isStarting = false, bool isEnding = false)
+        {
+            this.TokenValue = value;
+            this.IsStarting = isStarting;
+            this.IsEnding = isEnding;
+        }
+
+        public bool IsStarting { get; private set; }
+        public bool IsEnding { get; private set; }
+        public String TokenValue { get; private set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}{1}{2}",
+                this.IsStarting ? "[" : "-",
+                this.TokenValue,
+                this.IsEnding ? "]" : "-");
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Token;
+            if (null == other)
+            {
+                return false;
+            }
+            else 
+            {
+                return
+                    this.IsStarting == other.IsStarting &
+                    this.IsEnding == other.IsEnding &
+                    String.Equals(this.TokenValue, other.TokenValue);
+            }
+        }
+    }
+}
