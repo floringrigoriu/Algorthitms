28a8fc47a209529099114d67fe6141e1bb32fa62
Florin Grigoriu <florin_grigoriu@yahoo.com>
2020-06-02 22:26:54 -0700

    P2 - Jun 2nd 2020

---
+++ LeetCode2020/MonthlyJune/p2.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+ * Given linked list -- head = [4,5,1,9], which looks like following:
+ */
+
+public class p2 {
+    class Solution {
+        public void deleteNode(ListNode node) {
+            if(node == null || node.next == null){
+                throw new RuntimeException("invalid input");
+            }
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
